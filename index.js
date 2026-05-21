2a554183cbb55b5929f9a11630a49e01c03c9747
UriVt <al222310563@gmail.com>
2026-05-20 19:58:09 -0600

    server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola IDGS 9444444");
+const express = require ('express')
+
+const app = express();
+
+
+app.get('/', (req, res) =>{
+    res.send('hola mundo');
+});
+
+app.listen(3000,() =>{
+    console.log('escucha desde el puerto 3000');
+});
