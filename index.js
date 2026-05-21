4dbd43088f390028de23ee4e2d5f053cec922696
UriVt <al222310563@gmail.com>
2026-05-20 20:02:18 -0600

    serveeer

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hola IDGS 9444444");
+const express = require ('express')
+
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) =>{
+    res.send('hola mundo');
+});
+const PORT =process.env.PORT;
+
+app.listen(PORT,() =>{
+    console.log('escucha desde el puerto ${PORT}');
+});
